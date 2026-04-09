25b5da534ab7d7ddf43cf16d9a1c300374c9c45b
Kiptengwer <kiptengwer@Kiptengwers-MacBook-Pro.local>
2026-04-09 17:55:00 +0200

    feat: add StripeCardRepo

---
+++ repository/stripe_card_repo.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"errors"
+
+	"github.com/kakitucurrency/kakitu-wallet-server/models/dbmodels"
+	"gorm.io/gorm"
+)
+
+type StripeCardRepo struct {
+	DB *gorm.DB
+}
+
+// GetCardByAccount returns the card for a kakituAddress, or nil if none exists.
+func (r *StripeCardRepo) GetCardByAccount(kakituAddress string) (*dbmodels.StripeCard, error) {
+	var card dbmodels.StripeCard
+	err := r.DB.Where("kakitu_address = ?", kakituAddress).First(&card).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &card, nil
+}
+
+// CreateCard persists a newly issued Stripe card.
+func (r *StripeCardRepo) CreateCard(kakituAddress, cardholderID, cardID, addressID string) (*dbmodels.StripeCard, error) {
+	card := &dbmodels.StripeCard{
+		KakituAddress:      kakituAddress,
+		StripeCardholderID: cardholderID,
+		StripeCardID:       cardID,
+		AddressID:          addressID,
+	}
+	if err := r.DB.Create(card).Error; err != nil {
+		return nil, err
+	}
+	return card, nil
+}
